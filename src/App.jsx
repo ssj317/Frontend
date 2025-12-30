551bf6f663ae84ca886832fbf8a2e3cfb2c9377b
Sai Subham Jena <jenasaisubham8@gmail.com>
2025-12-30 14:09:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import CreatePaste from "./pages/CreatePaste";
 import ViewPaste from "./pages/ViewPaste";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<CreatePaste />} />
         <Route path="/p/:id" element={<ViewPaste />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
